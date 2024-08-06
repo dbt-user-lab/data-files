b86c22d3c93d4f72cbb5fd72da05e978b498cc8e
Satish <satishreddy.g@genpact.com>
2024-08-06 08:27:02 -0700

    day-2

--- DAY-2/models/reports/most_ordered_customers.sql
+++
@@ -1,8 +0,0 @@
-{{config(materialized='ephemeral')}}
-
-select CUSTOMER_ID, sum(ORDER_AMOUNT) as tot_amt,
-rank() over (order by sum(ORDER_AMOUNT)) as rnk
-from {{ref('DBT_TRAINING_DEMO','int_sales')}}
-group by CUSTOMER_ID
-qualify rnk <=5
-
--- DAY-2/tests/product_unit_price_test.sql
+++
@@ -1,5 +0,0 @@
---Test if product unit price is equal to orderamount/quantity
-select 
-    order_id,product_id,product_unit_price,quantity,order_amount
-from {{ ref('fact_sales') }}
-where product_unit_price !=(order_amount/quantity)
